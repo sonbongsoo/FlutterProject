874c74ec39ab8172515e78ccf19280ff105bcd7e
손봉수 <sonbs99@gmail.com>
2023-11-11 17:24:48 +0900

    FLUTTER FIRST COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  var a = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Text('번트'),
+        onPressed: () {
+          print(a);
+          a++;
+        },
+      ),
+      appBar: AppBar(),
+      body: ListView.builder(
+          itemCount: 3,
+        itemBuilder: (c, i) {
+          return ListTile(
+            leading: Image.asset('ddkfk'),
+            title: Text('sfdf'),
+          );
+        },
+      ),
+    ));
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안녕'),
+    );
+  }
+}
+
+class  extends StatelessWidget {
+  const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
